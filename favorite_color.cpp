055b38958f2181bac62a79e5ae82bf411d856a4d
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-09 23:51:56 +0500

    Create favorite_color.cpp

---
+++ favorite_color.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void color(string c) {
+    cout << "Favorite color: " << c << endl;
+}
+
+int main() {
+    color("Blue");
+    color("Green");
+
+    return 0;
+}
