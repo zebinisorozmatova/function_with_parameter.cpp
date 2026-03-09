01ff76c133f2ce88eb2859a37b161fefeb5a1932
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-09 23:51:16 +0500

    Create greet_function.cpp

---
+++ greet_function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void greet(string name) {
+    cout << "Hello, " << name << "!" << endl;
+}
+
+int main() {
+    greet("Ali");
+    greet("Vali");
+
+    return 0;
+}
